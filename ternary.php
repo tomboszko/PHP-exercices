46a1813a88910945a5d8eb4bd011989925ee15ed
tomboszko <tgomree@hotmail.com>
2023-10-19 14:58:33 +0200

    Exercice 11 (ternary)

---
+++ ternary.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ternary exercices</title>
+</head>
+<body>
+
+<h1>Ternary</h1>
+
+<!-- 11. Ternary exercises
+In a new file ternary.php declare a $hello variable 
+which value is a greeting message. That greeting message 
+depends on another variable $gender, which can either 
+be "M" or "F". Finish by displaying the resulting message. -->
+
+<?php
+
+// $gender = "F"; // choose between M or F
+
+// $hello = ($gender == "M") ? "Hello sir!" : "Hello madam!";
+// echo $hello;
+?>
+
+
+
+    
+</body>
+</html>
