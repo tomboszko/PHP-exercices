05f8c818dd67e9aef31e4ca95676902a23c2cae3
tomboszko <tgomree@hotmail.com>
2023-10-23 13:41:08 +0200

    Update functions exercice: all done

--- functions.php
+++ functions.php
@@ -124,35 +124,64 @@
 // echo feedback("This is an error message", "error");
 // echo feedback("This is a warning message", "warning");
 
-function generateWord($minLength, $maxLength) {
-    $length = rand($minLength, $maxLength);
-    $characters = 'abcdefghijklmnopqrstuvwxyz';
-    $word = '';
-    for ($i = 0; $i < $length; $i++) {
-        $index = rand(0, strlen($characters) - 1);
-        $word .= $characters[$index];
-    }
-    return $word;
-}
+// function generateWord($minLength, $maxLength) {
+//     $length = rand($minLength, $maxLength);
+//     $characters = 'abcdefghijklmnopqrstuvwxyz';
+//     $word = '';
+//     for ($i = 0; $i < $length; $i++) {
+//         $index = rand(0, strlen($characters) - 1);
+//         $word .= $characters[$index];
+//     }
+//     return $word;
+// }
 
-if (isset($_POST['generate'])) {
-    $word1 = generateWord(1, 5);
-    $word2 = generateWord(7, 15);
-} else {
-    $word1 = '';
-    $word2 = '';
-}
+// if (isset($_POST['generate'])) {
+//     $word1 = generateWord(1, 5);
+//     $word2 = generateWord(7, 15);
+// } else {
+//     $word1 = '';
+//     $word2 = '';
+// }
+
+// 
+?>
+
+ <!-- <h1>Generate a new word</h1>
+     <p>  <?php echo $word1; ?></p>
+     <p> <?php echo $word2; ?></p>
+     <form method="post">
+         <button type="submit" name="generate">Generate</button>
+     </form> -->
+
+<?php
+
+//     $str = "STOP YELLING I CAN'T HEAR MYSELF THINKING!!";
+// $str = strtolower($str);
+// echo $str; 
 
 
 ?>
 
-<h1>Generate a new word</h1>
-    <p><?php echo $word1; ?></p>
-    <p><?php echo $word2; ?></p>
-    <form method="post">
-        <button type="submit" name="generate">Generate</button>
-    </form>
-    
+<?php
+// Volume of a cone which ray is 5 and height is 2 
+$volume = 5 * 5 * 3.14 * 2 * (1/3);  
+echo 'The volume of a cone which ray is 5 and height is 2 = ' . $volume . ' cm<sup>3</sup><br />';  
+// Volume of a cone which ray is 3 and height is 4  
+$volume = 3 * 3 * 3.14 * 4 * (1/3);  
+echo 'The volume of a cone which ray is 3 and height is 4 = ' . $volume . ' cm<sup>3</sup><br />'; 
+
+
+function calculate_cone_volume($radius, $height) {
+    return $radius * $radius * 3.14 * $height * (1/3);
+}
+
+$volume1 = calculate_cone_volume(5, 2);
+echo 'The volume of a cone with a radius of 5 and height of 2 = ' . $volume1 . ' cm<sup>3</sup><br />';
+
+$volume2 = calculate_cone_volume(3, 4);
+echo 'The volume of a cone with a radius of 3 and height of 4 = ' . $volume2 . ' cm<sup>3</sup><br />';
+
+?>
 
 
 
