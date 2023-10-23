da2637acd20dc663a53160a95118171a23bc95bc
tomboszko <tgomree@hotmail.com>
2023-10-23 10:08:52 +0200

    update loop exercices

--- loops.php
+++ loops.php
@@ -6,7 +6,78 @@
     <title>PHP loops</title>
 </head>
 <body>
-<h1>PHP loops</h1>    
+<h1>PHP loops</h1>  
+
+<?php
+
+// $names= array('John', 'jeanne', 'Joan', 'émile');
+// foreach ($names as $name){
+// 	echo ucfirst($name);
+// }
+
+// echo '<pre>';
+//  print_r($names);
+//   echo '</pre>';
+
+//   echo '<pre>';
+//  print_r($name);
+//   echo '</pre>';
+
+//   $names= array('John', 'jeanne', 'Joan', 'émile');
+//   var_dump($names);
+  
+//   foreach ($names as $key => $value){
+//       $names[$key] = ucfirst($value);
+//   }
+//   echo '<pre>';
+//   var_dump($names);
+//   echo '</pre>';
+
+
+// $pronouns = array('I', 'You', 'He/She', 'We', 'You', 'They');
+
+// foreach ($pronouns as $pronoun) {
+//     // Check if the pronoun is "He/She"
+//     $verb = ($pronoun == 'He/She') ? 'codes' : 'code';
+
+//     // Display the conjugated verb and pronoun
+//     echo "$pronoun $verb<br>";
+// }
+
+// $amount_of_lines = 1;
+
+// while ($amount_of_lines <= 100)
+// {
+//     echo $amount_of_lines . ". : I shall not watch flies fly when I'm learning PHP.<br />";
+//     // shorthand writing for 
+//     // $amount_of_lines = $amount_of_lines +1;
+//     $amount_of_lines ++; 
+
+// }
+
+// for ($amount_of_lines = 1; $amount_of_lines <= 100; $amount_of_lines ++)
+// {
+//     echo $amount_of_lines . '. I shall not watch flies fly when I m learning PHP.<br />';
+// }
+
+
+// while ($numbers <= 120)
+// {
+//     echo $numbers . '<br />';
+//     $numbers ++;
+// }
+
+// for ($numbers = 1; $numbers <= 120; $numbers ++)
+// {
+//     echo $numbers . '<br />';
+// }
+
+
+
+
+
+?>
+
 
 
 
